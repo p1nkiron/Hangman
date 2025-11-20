d7563a23de0c0384246c7874f3649f009a5292e2
Злава <zlatakyhalova@gmail.com>
2025-11-20 14:03:54 +0500

    Create game.cpp

---
+++ game.cpp
@@ -0,0 +1,76 @@
+#include "h.h"
+#include <ctime>
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <cstdlib>
+using namespace std;
+void drawHangman(int triesLeft) {
+    int stage = 6 - triesLeft;
+    cout << " _______\n";
+    cout << " |     |\n";
+    cout << " |     " << (stage > 0 ? "O" : " ") << "\n";
+    cout << " |    "  << (stage > 2 ? "/" : " ")
+                     << (stage > 1 ? "|" : " ")
+                     << (stage > 3 ? "\\" : " ") << "\n";
+    cout << " |    "  << (stage > 4 ? "/" : " ")
+                     << " "
+                     << (stage > 5 ? "\\" : " ") << "\n";
+    cout << " |\n";
+    cout << "_|_\n\n";
+}
+
+void game(int tries,string guess,string word, string input) {
+    while (tries > 0 && guess != word) {
+        drawHangman(tries);
+        cout << "Слово: ";
+        for (char c : guess) cout << c << ' ';
+        cout << "\nОсталось попыток: " << tries << "\n";
+        cout << "Введите букву или слово: ";
+        cin >> input;
+
+        if (input.size() == 1) {
+            char letter = tolower(input[0]);
+            bool found = false;
+            for (int i = 0; i < word.size(); ++i) {
+                if (tolower(word[i]) == letter) {
+                    guess[i] = word[i];
+                    found = true;
+                }
+            }
+            if (found)
+                cout << "Хорошая работа!\n";
+            else {
+                cout << "Этой буквы нет.\n";
+                tries--;
+            }
+        } else {
+            if (input == word) {
+                guess = word;
+                break;
+            } else {
+                cout << "Неправильное слово!\n";
+                tries--;
+            }
+        }
+    }
+    if (guess == word) {
+        cout << "\nТы выиграл! Слово было: " << word << "\n";
+        cout << " /\\_/\\\n";
+        cout << "( o.o )\n";
+        cout << " > ^ <\n";
+    }
+    else{
+        cout << "\nТы проиграл. Слово было: " << word << "\n";
+    draw(tries);
+}
+}
+void draw(int tries) {
+    cout << " _______\n";
+    cout << " |     |\n";
+    cout << " |     0"<<"\n";
+    cout << " |    /|\\ "<<"\n";
+    cout << " |    / \\ " <<"\n";
+    cout << " |\n";
+    cout << "_|_\n\n";
+}
